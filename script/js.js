836a7b1d613e845e396aa1ca4d06a47147829f6a
s.zychkov <zychkov.serhii@gmail.com>
2022-12-22 18:31:03 +0200

    First commit

---
+++ script/js.js
@@ -0,0 +1,10 @@
+function showText (el) {
+	if(el.previousElementSibling.clientHeight === 80) {
+		el.previousElementSibling.style.height = "100%";
+		el.innerHTML = "ShowLess...";
+	} else {
+		el.previousElementSibling.style.height = "80px";
+		el.innerHTML = "Read More...";
+	}
+}
+id = document.
