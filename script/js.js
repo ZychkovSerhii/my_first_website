12b3160aa3a274c456c41727b5fc29ef593d90e0
s.zychkov <zychkov.serhii@gmail.com>
2022-12-22 18:55:12 +0200

    Second commit

--- script/js.js
+++ script/js.js
@@ -1,10 +1,9 @@
-function showText (el) {
-	if(el.previousElementSibling.clientHeight === 80) {
-		el.previousElementSibling.style.height = "100%";
-		el.innerHTML = "ShowLess...";
-	} else {
-		el.previousElementSibling.style.height = "80px";
-		el.innerHTML = "Read More...";
-	}
+function showText(el) {
+  if (el.previousElementSibling.clientHeight === 80) {
+    el.previousElementSibling.style.height = "100%";
+    el.innerHTML = "ShowLess...";
+  } else {
+    el.previousElementSibling.style.height = "80px";
+    el.innerHTML = "Read More...";
+  }
 }
-id = document.
